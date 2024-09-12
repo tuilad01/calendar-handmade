0fadb1da29db3093fab309b61c4042afe588755c
dat truong <thelegendaglx@gmail.com>
2024-09-12 10:13:44 +0700

    update 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent {
   selectedStartDayOfWeek = 0;
 
   constructor() {
-    this.selectedMonth = this.generateMonth(
+    this.selectedMonth = this.generateMonthByWeeks(
       this.displayMonth.month(),
       this.displayMonth.year(),
       this.selectedStartDayOfWeek,
@@ -44,6 +44,16 @@ export class AppComponent {
     this.selectedDayOfWeek = this.generateWeek(this.selectedStartDayOfWeek);
 
     console.log(this.generateWeek(this.selectedStartDayOfWeek));
+
+    console.log(
+      'generateMonth',
+      this.generateMonth(
+        this.displayMonth.month(),
+        this.displayMonth.year(),
+        this.selectedStartDayOfWeek,
+        false
+      )
+    );
   }
 
   generateWeek(startDayOfWeek: number) {
@@ -69,7 +79,7 @@ export class AppComponent {
     return week.map((dow) => dayOfWeek[dow]);
   }
 
-  generateMonth(
+  generateMonthByWeeks(
     month: number,
     year: number,
     startDayOfWeek: number = 1,
@@ -127,26 +137,103 @@ export class AppComponent {
     return list;
   }
 
+  generateMonth(
+    month: number,
+    year: number,
+    startDayOfWeek: number = 1,
+    hasOutRange: boolean = false
+  ) {
+    const list: any[] = [];
+
+    const selectedMonth = moment(new Date(year, month, 1));
+    const previousMonth = moment(selectedMonth).subtract(1, 'months');
+
+    console.log(selectedMonth.calendar(), previousMonth.calendar());
+
+    // from 26-8 - 25-9
+    const fromDate = moment(previousMonth).date(26);
+    const toDate = moment(selectedMonth).date(25);
+
+    let startDate = moment(fromDate);
+    while (startDate.day() != startDayOfWeek) {
+      startDate = moment(startDate).subtract(1, 'days');
+    }
+
+    let endDayOfWeek =
+      startDayOfWeek - 1 - 7 * Math.floor((startDayOfWeek - 1) / 7);
+    let endDate = moment(toDate);
+    while (endDate.day() != endDayOfWeek) {
+      endDate = moment(endDate).add(1, 'days');
+    }
+
+    let date = moment(startDate);
+    //let week: any[] = [];
+    const max = 90;
+    let count = 0;
+
+    while (date >= startDate && date <= endDate && count < max) {
+      // if (date.day() === startDayOfWeek) {
+      //   week = [];
+      // }
+
+      if (!hasOutRange && (date < fromDate || date > toDate)) {
+        list.push(null);
+      } else {
+        list.push(date.date());
+      }
+
+      // if (date.day() == endDayOfWeek) {
+      //   list.push(week);
+      // }
+
+      date = moment(date).add(1, 'days');
+
+      count++;
+    }
+
+    console.log(list);
+    return list;
+  }
+
   selecteDate(date: moment.Moment) {
     console.log(date.date());
   }
 
   previous() {
     this.displayMonth = moment(this.displayMonth).subtract(1, 'months');
-    this.selectedMonth = this.generateMonth(
+    this.selectedMonth = this.generateMonthByWeeks(
       this.displayMonth.month(),
       this.displayMonth.year(),
       this.selectedStartDayOfWeek,
       false
     );
+
+    console.log(
+      'generateMonth',
+      this.generateMonth(
+        this.displayMonth.month(),
+        this.displayMonth.year(),
+        this.selectedStartDayOfWeek,
+        false
+      )
+    );
   }
   next() {
     this.displayMonth = moment(this.displayMonth).add(1, 'months');
-    this.selectedMonth = this.generateMonth(
+    this.selectedMonth = this.generateMonthByWeeks(
       this.displayMonth.month(),
       this.displayMonth.year(),
       this.selectedStartDayOfWeek,
       false
+    );
+    console.log(
+      'generateMonth',
+      this.generateMonth(
+        this.displayMonth.month(),
+        this.displayMonth.year(),
+        this.selectedStartDayOfWeek,
+        false
+      )
     );
   }
 }
